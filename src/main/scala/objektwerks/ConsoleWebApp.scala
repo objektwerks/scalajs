18f03fb22a722e9753656ca5963d4ba71b5b241c
objektwerks <objektwerks@runbox.com>
2021-03-23 10:29:14 -0400

    refactored console web app

--- src/main/scala/objektwerks/ConsoleWebApp.scala
+++ src/main/scala/objektwerks/ConsoleWebApp.scala
@@ -1,13 +1,27 @@
 package objektwerks
 
+import org.scalajs.dom
 import org.scalajs.dom.document
 
 object ConsoleWebApp {
   def main(args: Array[String]): Unit = {
+    document.addEventListener("DOMContentLoaded", { (_: dom.Event) =>
+      init()
+    })
     println("*** Scalajs console app! ***")
+  }
+
+  def init(): Unit = {
     val pElement = document.createElement("p")
     pElement.textContent = "*** Scalajs web app! ***"
     document.body.appendChild(pElement)
+
+    val button = document.createElement("button")
+    button.textContent = "Click me!"
+    button.addEventListener("click", { (_: dom.MouseEvent) =>
+      println("*** Button clicked! ***")
+    })
+    document.body.appendChild(button)
     ()
   }
 }
